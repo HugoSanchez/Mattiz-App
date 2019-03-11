107a96e8a770b0ed146a070e431696142612d7f1
Hugo Sanchez <hugosanchez@Hugos-MacBook.local>
2019-03-11 21:57:31 +0100

    Worked on Signup flow

--- android/app/src/main/java/com/mattizapp/MainApplication.java
+++ android/app/src/main/java/com/mattizapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.mattizapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.tradle.react.UdpSocketsModule;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
             new UdpSocketsModule(),
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -12,10 +12,10 @@ import { setUser } from '../actions/AuthActions';
 class SignUpForm extends Component {
 
     state = {
-        username: '',
-        email: '',
+        username: null,
         password: '',
-        renderForm: false,
+        confirmPassword: '',
+        formStatus: false,
         isFocused: false
     }
 
@@ -34,8 +34,8 @@ class SignUpForm extends Component {
     }
 
     render() {
-        const {container, input, signUpButton } = styles;
-        if ( this.state.renderForm == false ) {
+        const {container, input, signUpButton, viewStyle } = styles;
+        if ( this.state.formStatus == false ) {
             return (
                 <View style={ container }>
                     <ImageBackground 
@@ -47,7 +47,7 @@ class SignUpForm extends Component {
                         title='Sign Up !'
                         titleStyle={{ color: '#03001A' }}
                         buttonStyle={ signUpButton }
-                        onPress={() => this.setState({ renderForm: true }) }
+                        onPress={() => this.setState({ formStatus: 'username' }) }
                         ViewComponent={LinearGradient}
                         linearGradientProps={{
                             colors: ['rgba(214, 213, 213, 1)', 'rgba(163, 209, 100, .4)'],
@@ -60,45 +60,30 @@ class SignUpForm extends Component {
             )
         } 
 
-        return (
-            <View style={ container }>
-                <ImageBackground 
-                    source={require('../assets/LoginBackground.png')} 
-                    style={{ width:'100%', height:'100%', flex: 1 }}
-                    resizeMode='cover'
-                >
-                    <View style={{ marginTop: 300 }}>
-                    <Input 
-                        placeholder='User ...'
-                        inputContainerStyle={{ borderBottomColor: 'white' }}
-                        placeholderTextColor='white'
-                        onChangeText={ value => this.setState({ username: value }) }
-                        fontStyle={ this.state.username.length == 0 ? 'italic' : 'normal' }
-                        onFocus={() => this.setState({ isFocused: true })}
-                        style={ input }
-                    />
-                    <Input 
-                        placeholder='Email ...'
-                        inputContainerStyle={{ borderBottomColor: 'white' }}
-                        placeholderTextColor='white'
-                        onChangeText={ value => this.setState({ email: value }) }
-                        fontStyle={ this.state.email.length == 0 ? 'italic' : 'normal' }
-                        style={ input }
-                    />
-                    <Input 
-                        placeholder='Password ...'
-                        inputContainerStyle={{ borderBottomColor: 'white' }}
-                        placeholderTextColor='white'
-                        onChangeText={ value => this.setState({ password: value }) }
-                        fontStyle={ this.state.password.length == 0 ? 'italic' : 'normal' }
-                        type='password'
-                        style={ input }
-                    />
+        if ( this.state.formStatus === 'username') {
+            return (
+                <View style={ container }>
+                    <ImageBackground 
+                        source={require('../assets/LoginBackground.png')} 
+                        style={{ width:'100%', height:'100%', flex: 1 }}
+                        resizeMode='cover'
+                    >
+                    <View style={ viewStyle }>
+                        <Input 
+                            placeholder="What's your name?"
+                            inputContainerStyle={{ borderBottomColor: 'transparent', marginTop: 5 }}
+                            placeholderTextColor='gray'
+                            onChangeText={ value => this.setState({ username: value }) }
+                            fontStyle={ this.state.password? 'italic' : 'normal' }
+                            onFocus={() => this.setState({ isFocused: true })}
+                            style={ input }
+                        />
+                    </View>
                     <Button 
-                        title='Sign Up !'
+                        title='Next !'
                         titleStyle={{ color: '#03001A' }}
-                        buttonStyle={{ margin: 12, marginTop: this.state.isFocused ? 10 : 300 }}
-                        onPress={ this.onButtonPress.bind(this) }
+                        buttonStyle={{ margin: 12, marginTop: this.state.isFocused ? 10 : 347, height: 47, backgroundColor: 'transparent' }}
+                        onPress={ () => this.setState({ formStatus: 'password' }) }
                         ViewComponent={LinearGradient}
                         linearGradientProps={{
                             colors: ['rgba(214, 213, 213, 1)', 'rgba(163, 209, 100, .4)'],
@@ -106,10 +91,82 @@ class SignUpForm extends Component {
                             end: { x:1, y: 0.5 },
                         }}
                     />
+                    </ImageBackground>
+                </View>
+            )
+        }
+        
+        if ( this.state.formStatus === 'password') {
+            return (
+                <View style={ container }>
+                    <ImageBackground 
+                        source={require('../assets/LoginBackground.png')} 
+                        style={{ width:'100%', height:'100%', flex: 1 }}
+                        resizeMode='cover'
+                    >
+                    <View style={ viewStyle }>
+                        <Input 
+                            placeholder="Choose your Password"
+                            inputContainerStyle={{ borderBottomColor: 'transparent', marginTop: 5 }}
+                            placeholderTextColor='gray'
+                            onChangeText={ value => this.setState({ password: value }) }
+                            fontStyle={ this.state.username ? 'italic' : 'normal' }
+                            onFocus={() => this.setState({ isFocused: true })}
+                            style={ input }
+                        />
                     </View>
-                </ImageBackground>
-            </View>
-        );
+                    <Button 
+                        title='Next !'
+                        titleStyle={{ color: '#03001A' }}
+                        buttonStyle={{ margin: 12, marginTop: this.state.isFocused ? 10 : 347, height: 47, backgroundColor: 'transparent' }}
+                        onPress={ () => this.setState({ formStatus: 'confirm' }) }
+                        ViewComponent={LinearGradient}
+                        linearGradientProps={{
+                            colors: ['rgba(214, 213, 213, 1)', 'rgba(163, 209, 100, .4)'],
+                            start: { x: 0.3, y: 0.5 },
+                            end: { x:1, y: 0.5 },
+                        }}
+                    />
+                    </ImageBackground>
+                </View>
+            )
+        }
+
+        if ( this.state.formStatus === 'confirm') {
+            return (
+                <View style={ container }>
+                    <ImageBackground 
+                        source={require('../assets/LoginBackground.png')} 
+                        style={{ width:'100%', height:'100%', flex: 1 }}
+                        resizeMode='cover'
+                    >
+                    <View style={ viewStyle }>
+                        <Input 
+                            placeholder="Confirm your Password"
+                            inputContainerStyle={{ borderBottomColor: 'transparent', marginTop: 5 }}
+                            placeholderTextColor='gray'
+                            onChangeText={ value => this.setState({ password: value }) }
+                            fontStyle={ this.state.username ? 'italic' : 'normal' }
+                            onFocus={() => this.setState({ isFocused: true })}
+                            style={ input }
+                        />
+                    </View>
+                    <Button 
+                        title='Sign Up'
+                        titleStyle={{ color: '#03001A' }}
+                        buttonStyle={{ margin: 12, marginTop: this.state.isFocused ? 10 : 347, height: 47, backgroundColor: 'transparent' }}
+                        onPress={ () => this.setState({ formStatus: 'confirm' }) }
+                        ViewComponent={LinearGradient}
+                        linearGradientProps={{
+                            colors: ['rgba(214, 213, 213, 1)', 'rgba(163, 209, 100, .4)'],
+                            start: { x: 0.3, y: 0.5 },
+                            end: { x:1, y: 0.5 },
+                        }}
+                    />
+                    </ImageBackground>
+                </View>
+            )
+        }
     }
 }
 
@@ -125,11 +182,18 @@ const styles = StyleSheet.create({
         margin: 30,
         padding: 20
     },
+    viewStyle: {
+        marginTop: 315, 
+        backgroundColor: 'rgba(255, 255, 255, 0.8)', 
+        borderRadius: 2,
+        margin: 12, 
+        height: 45
+    },
     signUpButton: {
         marginTop: 700,
         margin: 12, 
         backgroundColor: 'transparent',
-        height: 47
+        height: 45
     }
 })
 
