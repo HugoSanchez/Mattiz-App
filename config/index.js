ef61a6ce597c499f1376ee56876c81a1fceaa133
Hugo Sanchez <hugosanchez@MacBook.localdomain>
2019-04-27 19:30:07 +0200

    config is hiden

--- config/index.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    apiKey: "AIzaSyBOH4aSPUMZMFpZPcLKl6khubyRxA9m",
-    seed: 'paddle image include please olympic uncover scatter click joy excite nice soul'
-};
-
-export { config };
