865795dcd2a0a36cf6b877b4d8461507bc662df4
Hugo Sanchez <hugosanchez@Hugos-MacBook.local>
2019-09-18 10:48:53 +0200

    RN 0.60.0 is working for some reason

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import { AsyncStorage } from 'react-native';
 import axios from 'axios';
 
-const URL = 'http://192.168.100.30:3000/api'
+const URL = 'http://192.168.100.20:3000/api'
 
     // ASYNCSTORAGE TOKEN FUNCTIONS // 
 
