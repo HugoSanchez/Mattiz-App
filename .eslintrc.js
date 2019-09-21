eea2fb628905a85688032f40fc6c6cff0498fcde
16sheep <marjutubli@gmail.com>
2019-09-21 09:26:25 +0100

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
+'use strict'
+
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  "parser": "babel-eslint" ,
+  "plugins": [
+    "prettier",
+    "react"
+  ],
+  "rules": {
+    "prettier/prettier": [
+      "error"
+    ],
+    "react/prop-types": 0
+  },
+  "env": {
+    "node" : true,
+    "es6": true,
+    "browser": true,
+  },
+  "extends": [
+    // "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  "settings": {
+    "react": {
+      "version": "latest",
+    },
+  }
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
+  useTabs: true,
+  tabWidth: 4,
+  printWidth: 80,
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  semi: false
 };
