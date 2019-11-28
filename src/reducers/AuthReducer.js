6e3c33b1cd3526f8d8b064640a68c9caed7cdce1
Hugo Sanchez <hugosanchez251@gmail.com>
2019-11-28 11:53:56 +0100

    initial changes to prepare for smart contract wallet

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,24 +1,20 @@
-import { 
-    SET_TOKEN, 
-    SET_USER, 
-    LOGOUT_USER 
-} from '../actions/types';
+import {SET_TOKEN, SET_USER, LOGOUT_USER} from '../actions/types'
 
 const INITIAL_STATE = {
-    user: { name: 'Pepito', password: 'superSecretPassword' },
-    token: false, 
-    error: null
+	user: {name: 'Pepito', password: 'superSecretPassword'},
+	token: false,
+	error: null,
 }
 
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case SET_USER:
-            return { ...state, user: action.payload}
-        case SET_TOKEN:
-            return { ...state, token: action.payload };
-        case LOGOUT_USER:
-            return { ...INITIAL_STATE };
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case SET_USER:
+			return {...state, user: action.payload}
+		case SET_TOKEN:
+			return {...state, token: action.payload}
+		case LOGOUT_USER:
+			return {...INITIAL_STATE}
+		default:
+			return state
+	}
 }
